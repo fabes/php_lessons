cd74b10fa33e96928c2ab53c6a8168e81e205a9d
Fabion Stephens <f.o.stephens@gmail.com>
2014-11-02 16:18:25 -0600

    Added variables to lesson and rename a couple files

---
+++ forms/index.php
@@ -0,0 +1,35 @@
+<!doctype HTML>
+<html>
+	<head>
+		<title>
+			Learning to handle forms in PHP
+		</title>
+	</head>
+<body>
+<div>
+	<a href="edit.php">Edit Profile</a>
+</div>
+	<form name="profile_info" id="profile_info" method="POST" action="validation.php">
+		<div>
+			<label>Enter your first name:</label>
+			<input type="text" name="first_name">
+		</div>
+		<div>
+			<label>Enter your last name:</label>
+			<input type="text" name="last_name">
+		</div>
+		<div>
+			<label>Enter your email</label>
+			<input type="text" name="email">
+		</div>
+		<div>
+			<label>Enter a password</label>
+			<input type="password" name="password" >
+		</div>
+		<div>
+			<input type="submit" name="submit_button" value="Create Profile">
+		</div>
+		<!-- This is a HTML comment-->
+	</form>
+</body>
+</html>
---
+++ variables/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+  A variables is a data item that holds some value for later use in your program;
+  Variables is PHP starts with the $ followed by whatever you want to name your variable.
+  Here are some rules of naming/creating your variables in php:
+    - Should NOT start with a number or special characters except for the _ underscore
+    - Should NOT contain any spaces.
+    - Should NOT any special charaters.
+    - Should NOT be in all caps. eg. $FAVORITEGIRL
+    - Can be in camel case eg. $FavoriteGirl
+    - Can contain any amount of underscores eg. $i_love_my_spoogie;
+    
+  Here are some examples of variables
+*/
+$first_name = "Fabes"; //<-- this is a variable with a data type of STRING
+//A string means a series of characters/texts between double or single quotes"
+
+$age = 99; // <-- this is a variable with a data type of INTEGER
+//Integers are numbers 0-9 or negative numbers
+
+$truthy = TRUE; // <-- this is a variable with a data type of BOOLEAN
+//Boolean are true or false data types
+
+$last_name ="Staep";
+$email = "f.o.stephens@gmail.com";
