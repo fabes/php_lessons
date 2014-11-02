cc629ff10501a506c609f0add346244178329986
Fabion Stephens <f.o.stephens@gmail.com>
2014-11-02 15:48:48 -0600

    Change form actions from GET to POST

--- forms.php
+++ forms.php
@@ -32,32 +32,32 @@
 </html>
 <?php
 
-if(isset($_GET['submit_button'])){
+if(isset($_POST['submit_button'])){
 
 	//check if first name field is set or has a value
-	if($_GET['first_name'] != ''){
-		print "This is what was submitted for first name --> ".$_GET['first_name'];
+	if($_POST['first_name'] != ''){
+		print "This is what was submitted for first name --> ".$_POST['first_name'];
 	}else{
 		print "Please enter a value for your First name";
 	}//eof validating first name field
 
 	//Check if last name field is set  or has a value
-	if($_GET['last_name'] != ''){
-		print "<br/>This is what was submitted for last name --> ".$_GET['last_name'];
+	if($_POST['last_name'] != ''){
+		print "<br/>This is what was submitted for last name --> ".$_POST['last_name'];
 	}else{
 		print "<br/> Please enter a value for your last name";
 	}//eof vaildating last name field
 
 
 	//Check if email field is set  or has a value
-	if($_GET['email'] != ''){
-		print "<br/>This is what was submitted for email --> ".$_GET['email'];
+	if($_POST['email'] != ''){
+		print "<br/>This is what was submitted for email --> ".$_POST['email'];
 	}else{
 		print "<br/> Please enter a value for your email";
 	}//eof vaildating last name field
 
-	if($_GET['password'] != ''){
-		print "<br/>This is what was submitted for password --> ".$_GET['password'];
+	if($_POST['password'] != ''){
+		print "<br/>This is what was submitted for password --> ".$_POST['password'];
 	}else{
 		print "<br/> Please enter a value for your password";
 	}//eof vaildating last name field
