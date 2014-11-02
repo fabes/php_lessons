4851695b49966a7bc833cb2737097a6e266ca204
Fabion Stephens <f.o.stephens@gmail.com>
2014-11-02 15:31:46 -0600

    Added an email field to the form

--- forms.php
+++ forms.php
@@ -8,10 +8,16 @@
 <body>
 	<form name="profile_info" id="profile_info" method="POST">
 		<div>
+			<label>Enter your first name:</label>
 			<input type="text" name="first_name">
 		</div>
 		<div>
+			<label>Enter your last name:</label>
 			<input type="text" name="last_name">
+		</div>
+		<div>
+			<label>Enter your email</label>
+			<input type="text" name="email">
 		</div>
 		<div>
 			<input type="submit" name="submit_button" value="Create Profile">
@@ -36,6 +42,14 @@ if(isset($_GET['submit_button'])){
 		print "<br/>This is what was submitted for last name --> ".$_GET['last_name'];
 	}else{
 		print "<br/> Please enter a value for your last name";
+	}//eof vaildating last name field
+
+
+	//Check if email field is set  or has a value
+	if($_GET['email'] != ''){
+		print "<br/>This is what was submitted for email --> ".$_GET['email'];
+	}else{
+		print "<br/> Please enter a value for your email";
 	}//eof vaildating last name field
 
 }//eof check if submit button is pressed/is set
